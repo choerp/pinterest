16ba4d50e57ad20ce8826da6e3f8465e486e7fa7
choerp <choerp@naver.com>
2021-04-17 23:09:11 +0900

    (1) .gitignore file 생성 (2) .env 로 SECRET KEY 암호화 (3) .gitignore로 기본설정 + .env + __pycache__/ 및 기타 track이 필요없는 파일들 이그노어 설정 (4) pycache 삭제로 불필요 track 제

--- pinterest/settings.py
+++ pinterest/settings.py
@@ -11,16 +11,24 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os, environ
+
+env = environ.Env(
+    DEBUG = (bool, False)
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+environ.Env.read_env(
+    env_file = os.path.join(BASE_DIR, '.env')
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v7al(wr$3-li7k0vunn=ic)tw&1e(2n6-njaa7f*x^#o&hv@8*'
+SECRET_KEY = env('SECRET KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
