51c1a7735eda9e2ee8dda14544d3aff3d82f2dec
choerp <choerp@naver.com>
2021-04-17 21:32:18 +0900

    * first commit -  - 기본 세팅 진행

---
+++ accountapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from accountapp.views import hello_world
+
+appname = "accountapp"
+
+urlpatterns = [
+    path('hello_world/', hello_world, name='hello_world')
+    # parameter1 : url주소. // parameter2 : views의 함수 // parameter3 : name??
+]
