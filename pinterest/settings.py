97b76d73cd7d51c836dba070d7d9ab78cfb5e6cf
choerp <choerp@naver.com>
2021-04-17 23:35:39 +0900

    HTML 실습  (1) "templates > base.html" file root 생성  (2) views.py 에서 return 값을 render(request, "html")  (3) settings.py 에서 templates DIR 설정

--- pinterest/settings.py
+++ pinterest/settings.py
@@ -14,21 +14,21 @@ from pathlib import Path
 import os, environ
 
 env = environ.Env(
-    DEBUG = (bool, False)
+    DEBUG=(bool, False)
 )
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 environ.Env.read_env(
-    env_file = os.path.join(BASE_DIR, '.env')
+    env_file=os.path.join(BASE_DIR, '.env')
 )
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET KEY')
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'pinterest.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
