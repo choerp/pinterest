181e34a6690cf7bdb64822c6064078f3cc5ffbe4
choerp <choerp@naver.com>
2021-05-01 00:03:01 +0900

    * ch.16  - HTML 내부 문서에 form형식으로 url, method(post), 보안 token, input 등을 넣어준다.  - views.py 에서 해당 request.method가 post일때, get일때의 render return되는 애들을 명시해준다.  - HTML 내부 문서에, render context가 위치할 폼을 구성한다.

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 
 
 def hello_world(request):
-    return render(request, 'accountapp/hello_world.html')
+
+    if request.method == "POST":
+        return render(request, 'accountapp/hello_world.html', context={'text': 'Posted!!'})
+    else:
+        return render(request, 'accountapp/hello_world.html', context={'text': 'Get!!'})
