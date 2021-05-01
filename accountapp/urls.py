7210db47df7981b238f8f0ad3a07ddf3ccc1a835
choerp <choerp@naver.com>
2021-05-01 20:55:50 +0900

    * ch. 18  - list 로 만들어서, html file에서 for문 돌림  - views.py에서는 post상태로가면 새로고침할때마다 계속 리스트 아이템에 추가되므로, get으로 redirect하는 걸 반환해줌.

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from accountapp.views import hello_world
 
-appname = "accountapp"
+app_name = "accountapp"
 
 urlpatterns = [
-    path('hello_world/', hello_world, name='hello_world')
+    path('hello_world/', hello_world, name='hello_world'),
     # parameter1 : url주소. // parameter2 : views의 함수 // parameter3 : name??
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,9 +1,10 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from .models import HelloWorld
 
-def hello_world(request):
 
+def hello_world(request):
     if request.method == "POST":
 
         temp = request.POST.get('hello_world_input')
@@ -12,6 +13,7 @@ def hello_world(request):
         new_hello_world.text = temp
         new_hello_world.save()
 
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world})
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
-        return render(request, 'accountapp/hello_world.html', context={'text': 'Get!!'})
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
