1e4134069a4ac2fa5fad8aed041233c6bfaf3f68
choerp <choerp@naver.com>
2021-05-01 21:20:15 +0900

    * ch. 21  - views.py 에서 class CreateView 상속 개체 생성  - url.py에서 create/ 경로 만들고 해당 상속객체를 받는다.   : 이때 as_view()로 class를 def화 시켜준다.  - html에서 form구문으로 생성한다.   : 이때 form action은 {% url '상위앱:하위앱' %} 형식 가능

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from accountapp.views import hello_world
+from accountapp.views import hello_world, AccountCreateView
 
 app_name = "accountapp"
 
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
     # parameter1 : url주소. // parameter2 : views의 함수 // parameter3 : name??
+    path('create/', AccountCreateView.as_view(), name='create')
 ]
