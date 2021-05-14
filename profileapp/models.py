f5adbe79be0e2b3036e7b4dcbf5ccba915439f6c
choerp <choerp@naver.com>
2021-05-14 21:30:38 +0900

    * 32 ch.

--- profileapp/models.py
+++ profileapp/models.py
@@ -8,9 +8,3 @@ class Profile(models.Model):
     image = models.ImageField(upload_to='profile/', null=True)
     nickname = models.CharField(max_length=200, null=True, unique=True)
     message = models.CharField(max_length=100, null=True)
-
-    def form_valid(self, form):
-        temp_profile = form.save(commit=False)
-        temp_profile.user = self.request.user
-        temp_profile.save()
-        return super().form_valid(form)
